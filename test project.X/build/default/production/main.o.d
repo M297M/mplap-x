eb225347704e2b4473b4c803b42a54aff4eb85a5
M297M <Mayarmagdy609@gmail.com>
2021-09-11 15:17:40 +0400

    hello

---
+++ test project.X/build/default/production/main.o.d
@@ -0,0 +1,34 @@
+build/default/production/main.o.d build/default/production/main.o: main.c \
+ d:\mplap\ x\mplab\ blue\avr\avr\include\avr\io.h \
+ d:\mplap\ x\mplab\ blue\avr\avr\include\avr\sfr_defs.h \
+ d:\mplap\ x\mplab\ blue\avr\avr\include\inttypes.h \
+ d:\mplap\ x\mplab\ blue\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\mplap\ x\mplab\ blue\avr\avr\include\stdint.h \
+ D:/Mplap\ x/MpLab\ blue/packs/Microchip/ATmega_DFP/2.3.126/xc8/avr/include/avr/iom32a.h \
+ d:\mplap\ x\mplab\ blue\avr\avr\include\avr\portpins.h \
+ d:\mplap\ x\mplab\ blue\avr\avr\include\avr\common.h \
+ d:\mplap\ x\mplab\ blue\avr\avr\include\avr\version.h \
+ d:\mplap\ x\mplab\ blue\avr\avr\include\avr\fuse.h \
+ d:\mplap\ x\mplab\ blue\avr\avr\include\avr\lock.h
+
+d:\mplap\ x\mplab\ blue\avr\avr\include\avr\io.h:
+
+d:\mplap\ x\mplab\ blue\avr\avr\include\avr\sfr_defs.h:
+
+d:\mplap\ x\mplab\ blue\avr\avr\include\inttypes.h:
+
+d:\mplap\ x\mplab\ blue\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\mplap\ x\mplab\ blue\avr\avr\include\stdint.h:
+
+D:/Mplap\ x/MpLab\ blue/packs/Microchip/ATmega_DFP/2.3.126/xc8/avr/include/avr/iom32a.h:
+
+d:\mplap\ x\mplab\ blue\avr\avr\include\avr\portpins.h:
+
+d:\mplap\ x\mplab\ blue\avr\avr\include\avr\common.h:
+
+d:\mplap\ x\mplab\ blue\avr\avr\include\avr\version.h:
+
+d:\mplap\ x\mplab\ blue\avr\avr\include\avr\fuse.h:
+
+d:\mplap\ x\mplab\ blue\avr\avr\include\avr\lock.h:
---
+++ test project.X/main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: Mayar
+ *
+ * Created on September 11, 2021, 3:12 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
